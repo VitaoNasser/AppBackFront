fb77d00e15e48f13ef1a1f13975383368f8ff10f
Victor Nasser <victor.nasser.dev@gmail.com>
2023-07-18 23:43:03 -0300

    Add assets and homeScreen properties

--- AppBackFront/Feature/Home/Screen/HomeScreen.swift
+++ AppBackFront/Feature/Home/Screen/HomeScreen.swift
@@ -16,12 +16,67 @@ class HomeScreen: UIView {
         return view
     }()
     
+    lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "Vector")
+        return imageView
+    }()
+    
+    lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.clipsToBounds = true
+        searchBar.layer.cornerRadius = 20
+        searchBar.placeholder = "Pesquise pelo nome:"
+        return searchBar
+    }()
+    
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView()
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
+        // TODO: Register
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24)
+        collectionView.setCollectionViewLayout(layout, animated: false)
+        return collectionView
+    }()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        // TODO: Register
+        tableView.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
+        tableView.separatorStyle = .none
+
+        return tableView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        addViews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addViews() {
+        addSubview(viewBackground)
+        viewBackground.addSubview(logoImageView)
+        viewBackground.addSubview(searchBar)
+        viewBackground.addSubview(collectionView)
+        addSubview(tableView)
+    }
+    
+    func configConstraints() {
+        NSLayoutConstraint.activate([
+        
+        
+        ])
     }
     
 }
