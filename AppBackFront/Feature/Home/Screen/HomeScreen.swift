6ef49ac9f584053a5af1077ae31704eac5d417d9
Victor Nasser <victor.nasser.dev@gmail.com>
2023-07-18 22:50:29 -0300

    Create view homeScreen

---
+++ AppBackFront/Feature/Home/Screen/HomeScreen.swift
@@ -0,0 +1,27 @@
+//
+//  HomeScreen.swift
+//  AppBackFront
+//
+//  Created by Victor Nasser on 18/07/23.
+//
+
+import UIKit
+
+class HomeScreen: UIView {
+
+    lazy var viewBackground: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
